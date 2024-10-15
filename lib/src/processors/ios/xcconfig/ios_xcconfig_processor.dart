4bee7544bb560fe4c5b255646e69adb4a9bdf23f
Vladimir <ipzotovvova@gmail.com>
2024-10-16 00:47:49 +0300

    add BUNDLE_ID to xxconfig

--- lib/src/processors/ios/xcconfig/ios_xcconfig_processor.dart
+++ lib/src/processors/ios/xcconfig/ios_xcconfig_processor.dart
@@ -66,6 +66,8 @@ class IOSXCConfigProcessor extends StringProcessor {
       'ASSET_PREFIX': Variable(value: _flavorName),
       'BUNDLE_NAME': Variable(value: _flavor.app.name),
       'BUNDLE_DISPLAY_NAME': Variable(value: _flavor.app.name),
+      if (_flavor.ios?.bundleId != null)
+        'BUNDLE_ID': Variable(value: _flavor.ios!.bundleId),
     })
       ..addAll(
         _flavor.ios?.variables.where((_, variable) =>
